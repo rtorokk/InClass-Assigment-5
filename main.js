e0411fa0a5856e347a03a91dde3256d467f2f57b
rtorokk <rachy.torok@gmail.com>
2025-03-27 14:36:28 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import getPosts from './api.js';
+function displayPosts(posts) {
+const container =document.getElementById('postContainer');
+
+getPosts().then(posts => {
+    posts.slice(0,5).forEach(post => {
+        const div = document.createElement('div');
+        div.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
+        container.appendChild(div);
+    });
+})
+.catch(err => {
+    container.innerHTML = `<p style="color:red;">Failed to load posts:
+${err.message}</p>`;
+});
+}
+displayPosts();
